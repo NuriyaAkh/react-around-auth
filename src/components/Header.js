ff255a41fae1209bd1ac48452ed75669e3c2116d
Nuriya Akhmedova <nuriya_a@yahoo.com>
2022-07-31 11:50:27 -0400

    styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,17 @@ import logo from '../images/logo.svg';
 import React from 'react';
 import {Link, Route} from 'react-router-dom';
 
-const Header = ({onLogOut,email}) => {
-  const handleLogOut=()=>{
+const Header = ({onLogOut, email}) => {
+  const handleLogOut = () => {
     onLogOut();
-  }
+  };
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="logo Around US" />
-      <Route exact path="/">
-        
+        <Route exact path="/">
         <div className="header__container">
           <p className="header__user-email">{email}</p>
-
-          <button className="header__log-out"  
-            
-             onClick={handleLogOut} > 
+          <button className="header__log-out" onClick={handleLogOut}>
             Log Out
           </button>
         </div>
